6acdbc1bfea9a14d17f8abcf7f4470aebe8c5c5e
templewulf <templewulf+github@gmail.com>
2012-11-09 20:40:59 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Turtletodo::Application.config.secret_token = 'f41f91b8457556ea9e6bc863ed9cda47b73ff33729c3f89554434a52e4f8fb3fa4462dc224c555fafd09bddc281011eef3f4b16ac98aead785c65bf671182313'
