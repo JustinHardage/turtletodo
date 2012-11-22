251770c865cb14853e95d4845d19d9218b8ade6c
templewulf <templewulf+github@gmail.com>
2012-11-21 19:18:19 -0600

    Man, I don't even know. Made model fields accessible?

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,3 @@
 class Task < ActiveRecord::Base
-  attr_accessible :name, :rank
-  attr_protected :current_work_log, :create_date, :close_date
+  attr_accessible :name, :rank, :current_work_log, :create_date, :close_date
 end
